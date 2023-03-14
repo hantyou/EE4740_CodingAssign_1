7dd11b639ddd68f750bb2b511249f38276ecc546
Peiyuan Zhai - Server <peiyuanzhai@icdcs5.ewi.tudelft.nl>
2023-03-14 15:03:26 +0100

    update

--- Assign_2.m
+++ Assign_2.m
@@ -1,19 +1,20 @@
 clc,clear,close all;
-
+rng(990611)
 %% image read
 I=imread("MESSI_image.jpeg");
 I=rgb2gray(I);
+I=imresize(I,0.5);
 I=double(I)/255;
 [H,W]=size(I);
 figure,
 imshow(I);
 sigma_n=sqrt(1e-3);
-h=32;w=32; % patch height and width
+h=16;w=16; % patch height and width
 n=h*w;
 N_K=3;
 Ks=round(2.^linspace(8,10,N_K));
-Ks=round(1024.*[0.2, 0.4,0.6,0.8]);
-% Ks=round(1024.*[0.2, 0.25]);
+Ks=round(h*w.*[0.2, 0.4,0.6,0.8]);
+% Ks=round(h*w.*[0.2, 0.25]);
 N_K=length(Ks);
 h_group=H/h; w_group=W/w;
 I_Patches=cell(h_group,w_group);
@@ -28,157 +29,166 @@ if isempty(p)
     poolNum=min(w_group*h_group,16);
     parpool(poolNum);
 end
-%% OMP sampling and recovery exp
-D=dctmtx(h);
-method="OMP";
-[I_Recovered_OMP,I_Recovered_OMP_complete]=doExp(I,D,h,w,Ks,method);
-%% ISTA sampling and recovery exp
-D=dctmtx(h);
-method="ISTA";
-[I_Recovered_ISTA,I_Recovered_ISTA_complete]=doExp(I,D,h,w,Ks,method);
-
-%% Calculate Err OMP
-Err_OMP=zeros(N_K,1);
-for k=1:N_K
-    err=0;
-    for i=1:h_group
-        for j=1:w_group
-            err=err+norm(I_Recovered_OMP{i,j,k}-I_Patches{i,j},2);
+for lambda=[10]
+    %% OMP sampling and recovery exp
+    D=dctmtx(h);
+    method="OMP";
+    [I_Recovered_OMP,I_Recovered_OMP_complete]=doExp(I,D,h,w,Ks,method);
+    %% ISTA sampling and recovery exp
+    D=dctmtx(h);
+    method="ISTA";
+    [I_Recovered_ISTA,I_Recovered_ISTA_complete]=doExp(I,D,h,w,Ks,method,lambda);
+    
+    %% Calculate Err OMP
+    Err_OMP=zeros(N_K,1);
+    for k=1:N_K
+        err=0;
+        for i=1:h_group
+            for j=1:w_group
+                err=err+norm(I_Recovered_OMP{i,j,k}-I_Patches{i,j},2);
+            end
         end
+        Err_OMP(k)=err/h_group/w_group;
     end
-    Err_OMP(k)=err/h_group/w_group;
-end
-Err_OMP_db=mag2db(Err_OMP);
-
-%% Calculate Err ISTA
-Err_ISTA=zeros(N_K,1);
-for k=1:N_K
-    err=0;
-    for i=1:h_group
-        for j=1:w_group
-            err=err+norm(I_Recovered_ISTA{i,j,k}-I_Patches{i,j},2);
+    Err_OMP_db=mag2db(Err_OMP);
+    
+    %% Calculate Err ISTA
+    Err_ISTA=zeros(N_K,1);
+    for k=1:N_K
+        err=0;
+        for i=1:h_group
+            for j=1:w_group
+                err=err+norm(I_Recovered_ISTA{i,j,k}-I_Patches{i,j},2);
+            end
         end
+        Err_ISTA(k)=err/h_group/w_group;
     end
-    Err_ISTA(k)=err/h_group/w_group;
+    Err_ISTA_db=mag2db(Err_ISTA);
+    %% Plot results
+    gcf=figure;
+    for k=1:N_K
+        subplot(ceil(N_K/2),2,k);
+        imshow(I_Recovered_OMP_complete{k});
+        title(sprintf("S=%d, Err=%.4f dB",Ks(k),Err_OMP_db(k)));
+    end
+    fname="OMP recover results";
+    fname=strcat(fname," lambda ",num2str(lambda));
+    sgtitle("OMP recover results");
+    
+    fig_export=hgexport('factorystyle');
+    fig_export.Resolution=300;
+    fig_export.Format='png';
+    fig_export.Width=10;
+    fig_export.Height=5;
+    hgexport(gcf,fname,fig_export);
+%     savefig(gcf,fname);
+    close gcf;
+    
+    gcf=figure;
+    for k=1:N_K
+        subplot(ceil(N_K/2),2,k);
+        imshow(I_Recovered_ISTA_complete{k});
+        title(sprintf("S=%d, Err=%.4f dB",Ks(k),Err_ISTA_db(k)));
+    end
+    fname="ISTA recover results";
+    fname=strcat(fname," lambda ",num2str(lambda));
+    sgtitle("ISTA recover results");
+    hgexport(gcf,fname,fig_export);
+    % savefig(gcf,fname);
+    close gcf;
+    %% Plot Err comparison
+    gcf=figure;
+    plot(Ks,Err_OMP_db,'-*');
+    hold on ;
+    plot(Ks,Err_ISTA_db,'-^');
+    hold off
+    legend("OMP","ISTA");
+    fname="Error comparison of OMP and ISTA";
+    fname=strcat(fname," lambda ",num2str(lambda));
+    title("Error comparison of OMP and ISTA");
+    ylabel("MSE (dB)")
+    xlabel("Sparsity S")
+    fig_export.Width=5;
+    fig_export.Height=3;
+    hgexport(gcf,fname,fig_export);
+    % savefig(gcf,fname);
+    close gcf
+    %% Calculate PSNR OMP
+    PSNR_OMP=zeros(N_K,1);
+    for k=1:N_K
+        err=0;
+        for i=1:h_group
+            for j=1:w_group
+                err=err+psnr(I_Recovered_OMP{i,j,k},I_Patches{i,j});
+            end
+        end
+        PSNR_OMP(k)=err/h_group/w_group;
+    end
+    
+    %% Calculate PSNR ISTA
+    PSNR_ISTA=zeros(N_K,1);
+    for k=1:N_K
+        err=0;
+        for i=1:h_group
+            for j=1:w_group
+                err=err+psnr(I_Recovered_ISTA{i,j,k},I_Patches{i,j});
+            end
+        end
+        PSNR_ISTA(k)=err/h_group/w_group;
+    end
+    %% Plot PSNR results
+    gcf=figure;
+    for k=1:N_K
+        subplot(ceil(N_K/2),2,k);
+        imshow(I_Recovered_OMP_complete{k});
+        title(sprintf("S=%d, PSNR=%.4f dB",Ks(k),PSNR_OMP(k)));
+    end
+    fname="OMP recover results PSNR";
+    fname=strcat(fname," lambda ",num2str(lambda));
+    sgtitle("OMP recover results");
+    
+    fig_export=hgexport('factorystyle');
+    fig_export.Resolution=300;
+    fig_export.Format='png';
+    fig_export.Width=10;
+    fig_export.Height=5;
+    hgexport(gcf,fname,fig_export);
+    % savefig(gcf,fname);
+    close gcf;
+    
+    gcf=figure;
+    for k=1:N_K
+        subplot(ceil(N_K/2),2,k);
+        imshow(I_Recovered_ISTA_complete{k});
+        title(sprintf("S=%d, PSNR=%.4f dB",Ks(k),PSNR_ISTA(k)));
+    end
+    fname="ISTA recover results PSNR";
+    fname=strcat(fname," lambda ",num2str(lambda));
+    sgtitle("ISTA recover results");
+    hgexport(gcf,fname,fig_export);
+    % savefig(gcf,fname);
+    close gcf;
+    %% Plot PSNR comparison
+    gcf=figure;
+    plot(Ks,PSNR_OMP,'-*');
+    hold on ;
+    plot(Ks,PSNR_ISTA,'-^');
+    hold off
+    legend("OMP","ISTA",'Location','southeast');
+    fname="PSNR comparison of OMP and ISTA";
+    fname=strcat(fname," lambda ",num2str(lambda));
+    title("PSNR comparison of OMP and ISTA");
+    ylabel("PSNR (dB)")
+    xlabel("Sparsity S")
+    fig_export.Width=5;
+    fig_export.Height=3;
+    hgexport(gcf,fname,fig_export);
+    % savefig(gcf,fname);
+    close gcf
+    
+    
 end
-Err_ISTA_db=mag2db(Err_ISTA);
 save("data4plot.mat");
-%% Plot results
-gcf=figure;
-for k=1:N_K
-    subplot(ceil(N_K/2),2,k);
-    imshow(I_Recovered_OMP_complete{k});
-    title(sprintf("S=%d, Err=%.4f dB",Ks(k),Err_OMP_db(k)));
-end
-fname="OMP recover results";
-sgtitle("OMP recover results");
-
-fig_export=hgexport('factorystyle');
-fig_export.Resolution=300;
-fig_export.Format='png';
-fig_export.Width=10;
-fig_export.Height=5;
-hgexport(gcf,fname,fig_export);
-savefig(gcf,fname);
-close gcf;
-
-gcf=figure;
-for k=1:N_K
-    subplot(ceil(N_K/2),2,k);
-    imshow(I_Recovered_ISTA_complete{k});
-    title(sprintf("S=%d, Err=%.4f dB",Ks(k),Err_ISTA_db(k)));
-end
-fname="ISTA recover results";
-sgtitle("ISTA recover results");
-hgexport(gcf,fname,fig_export);
-savefig(gcf,fname);
-close gcf;
-%% Plot Err comparison
-gcf=figure;
-plot(Ks,Err_OMP_db,'-*');
-hold on ;
-plot(Ks,Err_ISTA_db,'-^');
-hold off
-legend("OMP","ISTA");
-fname="Error comparison of OMP and ISTA";
-title("Error comparison of OMP and ISTA");
-ylabel("MSE (dB)")
-xlabel("Sparsity S")
-fig_export.Width=5;
-fig_export.Height=3;
-hgexport(gcf,fname,fig_export);
-savefig(gcf,fname);
-close gcf
-%% Calculate PSNR OMP
-PSNR_OMP=zeros(N_K,1);
-for k=1:N_K
-    err=0;
-    for i=1:h_group
-        for j=1:w_group
-            err=err+psnr(I_Recovered_OMP{i,j,k},I_Patches{i,j});
-        end
-    end
-    PSNR_OMP(k)=err/h_group/w_group;
-end
-
-%% Calculate PSNR ISTA
-PSNR_ISTA=zeros(N_K,1);
-for k=1:N_K
-    err=0;
-    for i=1:h_group
-        for j=1:w_group
-            err=err+psnr(I_Recovered_ISTA{i,j,k},I_Patches{i,j});
-        end
-    end
-    PSNR_ISTA(k)=err/h_group/w_group;
-end
-save("data4plot.mat");
-%% Plot PSNR results
-gcf=figure;
-for k=1:N_K
-    subplot(ceil(N_K/2),2,k);
-    imshow(I_Recovered_OMP_complete{k});
-    title(sprintf("S=%d, PSNR=%.4f dB",Ks(k),PSNR_OMP(k)));
-end
-fname="OMP recover results PSNR";
-sgtitle("OMP recover results");
-
-fig_export=hgexport('factorystyle');
-fig_export.Resolution=300;
-fig_export.Format='png';
-fig_export.Width=10;
-fig_export.Height=5;
-hgexport(gcf,fname,fig_export);
-savefig(gcf,fname);
-close gcf;
-
-gcf=figure;
-for k=1:N_K
-    subplot(ceil(N_K/2),2,k);
-    imshow(I_Recovered_ISTA_complete{k});
-    title(sprintf("S=%d, PSNR=%.4f dB",Ks(k),PSNR_ISTA(k)));
-end
-fname="ISTA recover results PSNR";
-sgtitle("ISTA recover results");
-hgexport(gcf,fname,fig_export);
-savefig(gcf,fname);
-close gcf;
-%% Plot PSNR comparison
-gcf=figure;
-plot(Ks,PSNR_OMP,'-*');
-hold on ;
-plot(Ks,PSNR_ISTA,'-^');
-hold off
-legend("OMP","ISTA",'Location','southeast');
-fname="PSNR comparison of OMP and ISTA";
-title("PSNR comparison of OMP and ISTA");
-ylabel("PSNR (dB)")
-xlabel("Sparsity S")
-fig_export.Width=5;
-fig_export.Height=3;
-hgexport(gcf,fname,fig_export);
-savefig(gcf,fname);
-close gcf
 %%
 function [s_hat,H_hat,idices] = OMP(y,H,sigma_n)
 [M,N]=size(H);
@@ -192,7 +202,7 @@ iterNum=0;
 H_i=zeros(size(H));
 while iterflag
     iterNum=iterNum+1;
-    %%% OMP goes here 
+    %%% OMP goes here
     r=abs(r_i'*H); % calculate correlation
     idx=find(r==max(r)); % find maximum idx
     idices(idx)=1;
@@ -208,7 +218,7 @@ H_hat=H_i;
 s_hat=pinv(H_hat)*y;
 end
 
-function [s_hat,H_hat,idices] = ISTA(y,H,sigma_n)
+function [s_hat,H_hat,idices] = ISTA(y,H,lambda,sigma_n)
 [M,N]=size(H);
 maxIter=N;
 iterflag=1;
@@ -223,7 +233,6 @@ J_D=abs(diag(J_D));
 beta=J_D(1);
 ita=1/2/beta;
 
-lambda=10;
 
 s_hat=zeros(N,1);
 p=zeros(N,1);
@@ -260,7 +269,10 @@ for i=1:a
 end
 end
 
-function [I_Recovered,I_Recovered_complete]=doExp(I,D,h,w,Ks,method)
+function [I_Recovered,I_Recovered_complete]=doExp(I,D,h,w,Ks,method,lambda)
+if nargin<=6
+    lambda=10;
+end
 [H,W]=size(I);
 sigma_n=sqrt(1e-3);
 N_K=length(Ks);
@@ -276,15 +288,16 @@ for i=1:h_group
     end
 end
 % Take Sparse Measurement and recover
-    U_kron=kron(D,D);
-    I_Recovered=cell(h_group,w_group,N_K);
-    I_Recovered_complete=cell(N_K,1);
+U_kron=kron(D,D);
+I_Recovered=cell(h_group,w_group,N_K);
+I_Recovered_complete=cell(N_K,1);
 for k=1:N_K
     K=Ks(k);
     D_m=[];
     for p=1:K
-        d_m=vec(randi([-1 1],h,w));
-        D_m=[D_m;d_m'];% Take several measurement 
+        d_m=vec(randi([0 1],h,w));
+        d_m(d_m==0)=-1;
+        D_m=[D_m;d_m'];% Take several measurement
     end
     H=D_m*U_kron; % concatenated everything into a measurement matrix
     
@@ -299,7 +312,7 @@ for k=1:N_K
         if method=="OMP"
             [J_hat_vec_par{iter},~,~] = OMP(y,H,sigma_n);
         else
-            [J_hat_vec_par{iter},~,~] = ISTA(y,H,sigma_n)
+            [J_hat_vec_par{iter},~,~] = ISTA(y,H,lambda,sigma_n)
         end
         J_hat_vec_par{iter}=reshape(J_hat_vec_par{iter},[h,w]);
         I_Recovered_par_k{iter}=D*J_hat_vec_par{iter}*D';
@@ -322,10 +335,10 @@ for k=1:N_K
         I_Recovered_complete{k}=[I_Recovered_complete{k};I_Recovered_complete_row];
     end
 end
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
 end
